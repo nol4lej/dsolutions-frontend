b579cef6483ee5901e9712b1969f5edec0ec312e
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-03-11 13:57:28 -0300

    changed: cambios y añadidos mas tipos a usuarios

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,8 +1,23 @@
-import { UserState } from "@/enums"
-
 export type User = {
-    firstName: string
-    lastName: string
-    email: string
-    status: UserState
-}
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
+};
